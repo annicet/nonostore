7366f0465be9882ffe65fececb02f8427ec9d97a
Drephyx Yango <dyango@dynamite.ca>
2024-03-15 09:18:17 -0400

    wip

---
+++ src/screens/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactComponentElement, ReactElement } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Login: React.FC = ():ReactElement => {
+    return(
+            <Text>
+                Login page
+            </Text>
+    )
+}
+
+export default Login;
